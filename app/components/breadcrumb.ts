0a3b46b159f9fe15a93e203facf111c9656fe5d6
Eduardo Albelo <eduardoalbelo@gmail.com>
2017-07-26 10:31:20 -0300

    Move router subscription to constructor

--- app/components/breadcrumb.ts
+++ app/components/breadcrumb.ts
@@ -23,28 +23,25 @@ export class BreadcrumbComponent implements OnInit, OnChanges {
     @Input() useBootstrap: boolean = true;
     @Input() prefix:       string  = '';
 
-    public _urls: string[];
+    public _urls: string[] = new Array();
     public _routerSubscription: any;
 
     constructor(
         private router: Router,
         private breadcrumbService: BreadcrumbService
-    ) {}
-
-    ngOnInit(): void {
-        this._urls = new Array();
-
-        if (this.prefix.length > 0) {
-            this._urls.unshift(this.prefix);
-        }
-
-        this._routerSubscription = this.router.events.subscribe((navigationEnd:NavigationEnd) => {
-
-           if (navigationEnd instanceof NavigationEnd) {
+    ) {
+        this._routerSubscription = this.router.events.subscribe((navigationEnd: NavigationEnd) => {
+            if (navigationEnd instanceof NavigationEnd) {
                 this._urls.length = 0; //Fastest way to clear out array
                 this.generateBreadcrumbTrail(navigationEnd.urlAfterRedirects ? navigationEnd.urlAfterRedirects : navigationEnd.url);
             }
         });
+    }
+
+    ngOnInit(): void {
+        if (this.prefix.length > 0) {
+            this._urls.unshift(this.prefix);
+        }
     }
 
     ngOnChanges(changes: any): void {
